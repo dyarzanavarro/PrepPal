ecc60ed311881b3d6414f9f4e90f32803e6a6e55
Dan <danielyarza1@gmail.com>
2025-01-19 11:39:49 +0100

    test pwa

--- app.vue
+++ app.vue
@@ -4,20 +4,10 @@
       <base-navigation />
       <NuxtPage />
     </NuxtLayout>
-    <button
-      v-if="showInstallButton"
-      @click="installApp"
-      class="fixed bottom-4 right-4 bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg"
-    >
-      Install App
-    </button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { usePwaInstall } from "./composables/usePwaInstall";
-
-const { showInstallButton, installApp } = usePwaInstall();
 provideHeadlessUseId(() => useId());
 </script>
 
